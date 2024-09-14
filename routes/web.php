fc1e4d571efe877cace1037531bc1232b67113c0
putrinuranisa <putrinuranisaaa044@gmail.com>
2024-09-14 12:12:32 +0700

    Latihan modul 2, add home, login, register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginRegisterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+    });
+
+Route::get('/auth/login', [LoginRegisterController::class, 'login'])->name('auth.login');
+
+Route::get('/auth/register', [LoginRegisterController::class,'register'])->name('auth.register');
